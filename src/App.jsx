034bb7f2c8864b6564b2f3e3bed3870d89f6523b
VinaySinghKadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-05-31 15:42:53 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="w-screen h-screen bg-red-100 ">
+      <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5 ">
+        <a className="py-3 px-5 border rounded" href=".create">
+          {" "}
+          Add new Product
+        </a>
+        <hr className="w-[80%] my-3" />
+        <h1>Category</h1>
+        <ul>
+          <li>cat 1</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
